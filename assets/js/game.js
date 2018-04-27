27675deb981640b0632082679223bbfc87b596bc
AMG <alexgood1@gmail.com>
2018-04-27 02:31:18 -0700

    completed files

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,29 +1,71 @@
-var alphabet = [a, b, c, d, e, f, g, h, i, j, k, l ,m, n, o, p, q, r, s, t, u, v, w, x, y, z];
+var guessesLeft = 0;
+var lettersUsed = [];
+var losses = 0;
+var max_guesses = 9;
+var solution = '';
+var wins = 0;
 
-var letterToGuess = 
-
-// Display the user letters guessed so far
-
-lettersGuessed = {
-    
+// returns a random letter from the alphabet
+function getRandomLetter() {
+    var code = Math.floor(Math.random() * (26)) + 97;
+    return String.fromCharCode(code);
 }
-function logArray(list) {
-    for (var j = 0; j < list.length; j++) {
-      console.log(list[j]);
+
+// user's guess - conditional code
+function userGuess(guessedLetter) {
+    // compare the user's letter with secret letter and previous guessed letters
+    if (lettersUsed.indexOf(guessedLetter) > -1) {
+        alert('You already used ' + guessedLetter + '.');
+    } else if (guessedLetter === solution) {
+        wins++;
+        newRound();
+    } else {
+        guessesLeft--;
+        lettersUsed.push(guessedLetter);
     }
-  }
 
+    // all 9 guesses used, game is lost, game is over
+    if (guessesLeft === 0) {
+        losses++;
+        newRound();
+    }
 
+    // update display
+    updateView(wins, losses, guessesLeft, lettersUsed);
+}
 
-//Display # of rounds won by the user, increasing with each winning round and resetting to 0 when the user reaches 10 wins without page refresh
+// resets values for new round
+function newRound() {
+    guessesLeft = max_guesses;
+    solution = getRandomLetter();
+    lettersUsed = [];
+}
 
-var i = 0;
+// updates data displayed on page
+function updateView(wins, losses, remGuesses, arrLetters) {
+    var lettersUsedElement = document.getElementById('letters-used');
+    lettersUsedElement.innerHTML = '';
+    arrLetters.forEach(function (letter) {
+        appendLetterElement(letter, lettersUsedElement);
+    });
+    // document.querySelector('#letters-used').textContent = arrLetters.join(', ');
+    document.querySelector('#wins').textContent = wins;
+    document.querySelector('#losses').textContent = losses;
+    document.querySelector('#guesses-left').textContent = remGuesses;
+}
 
+// appends letter element
+function appendLetterElement(letter, container) {
+    container.innerHTML = container.innerHTML + '<span class="letter label label-default text-uppercase">' + letter + '</span>';
+}
 
-var userGuess = alphabet[i];
+// game starts when key is pressed
+document.onkeyup = function (event) {
+    // key pressed is a to z inclusive
+    if (event.keyCode >= 65 && event.keyCode <= 90) {
+        userGuess (event.key.toLowerCase());
+    }
+};
 
-if (userGuess === )
-
-//Display # of rounds lost by the user, increasing with each losing round and resetting to 0 when the user reaches 10 losses without page refresh
-
-
+// start new game
+newRound();
