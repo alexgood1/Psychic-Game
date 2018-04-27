5b094c61bafde5bf7725f3e60f94e9fff5ef9a6f
AMG <alexgood1@gmail.com>
2018-04-26 23:34:26 -0700

    code update

---
+++ assets/js/game.js
@@ -0,0 +1,29 @@
+var alphabet = [a, b, c, d, e, f, g, h, i, j, k, l ,m, n, o, p, q, r, s, t, u, v, w, x, y, z];
+
+var letterToGuess = 
+
+// Display the user letters guessed so far
+
+lettersGuessed = {
+    
+}
+function logArray(list) {
+    for (var j = 0; j < list.length; j++) {
+      console.log(list[j]);
+    }
+  }
+
+
+
+//Display # of rounds won by the user, increasing with each winning round and resetting to 0 when the user reaches 10 wins without page refresh
+
+var i = 0;
+
+
+var userGuess = alphabet[i];
+
+if (userGuess === )
+
+//Display # of rounds lost by the user, increasing with each losing round and resetting to 0 when the user reaches 10 losses without page refresh
+
+
